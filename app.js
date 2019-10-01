a78232aeb3547832500c462eb1387405410c25f5
DAMILOLA <jogungbure@gmail.com>
2019-10-01 11:19:37 +0100

    Setting up the express handlebars... exphbs

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
+
+// Setting up the handlebars middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('INDEX');
+});
+
+// About Route
+app.get('/about', (req, res) => {
+  res.send('ABOUT');
+});
 
 // Port connection setup
 const port = 3000;
