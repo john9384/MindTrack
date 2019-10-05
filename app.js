c675633146ba496d4cc5a05ec28bd78d320ebc7c
DAMILOLA <jogungbure@gmail.com>
2019-10-05 18:04:18 +0100

    Adding validation to the idea form

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const mongoose = require('mongoose');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,43 +9,74 @@ const app = express();
 // mongoose.Promise = globalPromise;
 
 //Connecting to the mongoose DB
-mongoose.connect('mongodb://localhost/mindtrack',{
-  //useMongoClient: true
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-
-}).then(() => console.log("MongoDB Connected...")).catch(err => console.log(err));
+mongoose
+  .connect("mongodb://localhost/mindtrack", {
+    //useMongoClient: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 
 // Loading the Idea Model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
+require("./models/Idea");
+const Idea = mongoose.model("ideas");
 
 // Setting up the handlebars middleware
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Route to index
-app.get('/', (req, res) => {
-  const title = 'Hello There';
-  res.render('index', {
+app.get("/", (req, res) => {
+  const title = "Hello There";
+  res.render("index", {
     title: title
   });
 });
 
 // Route to about
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 //Route to the Idea form
-app.get('/ideas/add', (req, res) => {
-  res.render('ideas/add');
-  });
+app.get("/ideas/add", (req, res) => {
+  res.render("ideas/add");
+});
+
+// Processing the Ideas from the form
+app.post("/ideas", (req, res) => {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({ text: "Please add a title" });
+  }
+  if (!req.body.details) {
+    errors.push({ text: "Please add some details" });
+  }
+
+  if (errors.length > 0) {
+    res.render("ideas/add", {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send("passed");
+  }
+});
 
 // Port connection setup
 const port = 3000;
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
